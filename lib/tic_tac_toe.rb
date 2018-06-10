25d923fd7cf7ec48b6470f90b9f7a6ae6aed88a0
Josh Mulloy <joshmulloy1@gmail.com>
2018-06-10 23:04:33 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -85,6 +85,6 @@ end
 false; #explicitly tell ruby to return false if we've cycled through the board and no win combinations can be found
 end
   def full?
-@board.all? {|position| position_taken?(@board, @board.index(position))}
+@board.all? {|position| position_taken?(@board.index(position))}
 end
 end
