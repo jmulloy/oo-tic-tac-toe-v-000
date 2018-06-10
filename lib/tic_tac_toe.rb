67fb6a7f927e98a492cd5f0f57a02a6cbbea4295
Josh Mulloy <joshmulloy1@gmail.com>
2018-06-10 22:18:59 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -40,4 +40,14 @@ class TicTacToe
 end
    counter
 end
+def current_player(board)
+#if turn_count(board) %  == 0
+  num = turn_count(board)
+  if num % 2 == 0
+    return "X"
+  else
+    return "O"
+  #puts "divisible by 2"
+  end
+end
 end
